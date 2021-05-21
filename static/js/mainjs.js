f57dd7fd4fb8fc079795f0c04ea39bb9aec03068
mccndcg <20100931@s.ubaguio.edu>
2021-05-21 16:50:06 +0800

    'imdone'

--- static/js/mainjs.js
+++ static/js/mainjs.js
@@ -1,7 +1,7 @@
 
 
 const axios_instance = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://project-database-bunbun.herokuapp.com/',
   timeout: 1000,
   headers: {
     'X-Custom-Header': 'foobar',
